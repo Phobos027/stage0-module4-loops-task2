9979f0545fb1ec28d33acf4789a7cdc67c159083
Phobos027 <shkuro.stepan@gmail.com>
2022-09-29 15:54:57 +0300

    done

--- src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
@@ -2,6 +2,19 @@ package school.mjc.stage0.loops.task2;
 
 public class FactorialNumbers {
     public void printFactorialRow(int printToInclusive) {
+        int factorial = 1;
+        int count = 0;
+        while (count <= printToInclusive){
+            int check = 1;
+            factorial = 1;
+            while (check <= count) {
+                factorial *= check;
+                check++;
+            }
+            System.out.println(factorial);
+            count++;
 
+        }
+        }
     }
-}
+
